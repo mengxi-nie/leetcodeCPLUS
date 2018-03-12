ce71709a2ad319ae5eee75d875cc299777fd0583
mengxi nie <eva_happylife@gwu.edu>
2018-03-11 21:49:45 -0400

    add

---
+++ 276-Paint Fence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numWays(int n, int k) {
+        if(n == 0) return 0;
+        if(n == 1) return k;
+        int way[n+1] = {0};
+        way[0] = 0;
+        way[1] = k;
+        way[2] = k * k;
+        for(int i = 3; i < n+1; i++) 
+            way[i] = (k-1) * (way[i-1] + way[i-2]);
+        return way[n];
+    }
+};
